f004c7c193428f4dc78b8bc93ddf8400d9aeb03d
Azmain Adhian <39245721+azmainasd@users.noreply.github.com>
2020-09-23 19:22:16 +0600

    Add files via upload

---
+++ wfaPhoneBook/wafPhoneBook/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace wafPhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvPhoneBookList = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contact_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhoneBookList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.dgvPhoneBookList);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.lblAddress);
+            this.panel1.Controls.Add(this.lblEmail);
+            this.panel1.Controls.Add(this.lblNumber);
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.txtAddress);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtNumber);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(864, 641);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.LightYellow;
+            this.btnCancel.Location = new System.Drawing.Point(694, 269);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 35);
+            this.btnCancel.TabIndex = 21;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.LightYellow;
+            this.btnEdit.Location = new System.Drawing.Point(19, 457);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(76, 36);
+            this.btnEdit.TabIndex = 20;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightYellow;
+            this.btnAdd.Location = new System.Drawing.Point(19, 316);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(76, 35);
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.Text = "Add+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(19, 589);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(76, 36);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(19, 274);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(252, 22);
+            this.txtSearch.TabIndex = 15;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9.2F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(288, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Search";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnSave.Location = new System.Drawing.Point(775, 268);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 35);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvPhoneBookList
+            // 
+            this.dgvPhoneBookList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPhoneBookList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.contact_number,
+            this.email,
+            this.address});
+            this.dgvPhoneBookList.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dgvPhoneBookList.Location = new System.Drawing.Point(111, 316);
+            this.dgvPhoneBookList.Name = "dgvPhoneBookList";
+            this.dgvPhoneBookList.ReadOnly = true;
+            this.dgvPhoneBookList.RowHeadersWidth = 51;
+            this.dgvPhoneBookList.RowTemplate.Height = 24;
+            this.dgvPhoneBookList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPhoneBookList.Size = new System.Drawing.Size(739, 309);
+            this.dgvPhoneBookList.TabIndex = 9;
+            this.dgvPhoneBookList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPhoneBookList_CellContentClick);
+            this.dgvPhoneBookList.DoubleClick += new System.EventHandler(this.dgvPhoneBookList_DoubleClick);
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.name.DataPropertyName = "name";
+            this.name.HeaderText = "Name";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // contact_number
+            // 
+            this.contact_number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.contact_number.DataPropertyName = "contact_number";
+            this.contact_number.HeaderText = "Phone Number";
+            this.contact_number.MinimumWidth = 6;
+            this.contact_number.Name = "contact_number";
+            this.contact_number.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.email.DataPropertyName = "email";
+            this.email.HeaderText = "Email";
+            this.email.MinimumWidth = 6;
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // address
+            // 
+            this.address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.address.DataPropertyName = "address";
+            this.address.HeaderText = "Address";
+            this.address.MinimumWidth = 6;
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(641, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 165);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.BackColor = System.Drawing.Color.Transparent;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft YaHei", 9.2F);
+            this.lblAddress.ForeColor = System.Drawing.Color.White;
+            this.lblAddress.Location = new System.Drawing.Point(48, 177);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(70, 21);
+            this.lblAddress.TabIndex = 7;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft YaHei", 9.2F);
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(48, 130);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(58, 21);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.BackColor = System.Drawing.Color.Transparent;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft YaHei", 9.2F);
+            this.lblNumber.ForeColor = System.Drawing.Color.White;
+            this.lblNumber.Location = new System.Drawing.Point(48, 84);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(127, 21);
+            this.lblNumber.TabIndex = 5;
+            this.lblNumber.Text = "Phone Number";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft YaHei", 9.2F);
+            this.lblName.ForeColor = System.Drawing.Color.White;
+            this.lblName.Location = new System.Drawing.Point(48, 42);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(56, 21);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Enabled = false;
+            this.txtAddress.Location = new System.Drawing.Point(192, 172);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(324, 35);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Enabled = false;
+            this.txtEmail.Location = new System.Drawing.Point(192, 127);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(324, 22);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Enabled = false;
+            this.txtNumber.Location = new System.Drawing.Point(192, 84);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(324, 22);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(192, 42);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(324, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 637);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Phone Book";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhoneBookList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvPhoneBookList;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contact_number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ wfaPhoneBook/wafPhoneBook/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wafPhoneBook
+{
+    public partial class Form1 : Form
+    {
+        DataAccess getData = new DataAccess();
+
+        internal DataAccess GetData
+        {
+            get { return this.getData; }
+            set { this.getData = value; }
+        }
+
+        internal string Sql { set; get; }
+        internal DataSet Ds { set; get; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.PopulateGridView();
+
+        }
+
+        private void PopulateGridView(string query = "select * from phonebook;")
+        {
+            this.Ds = this.GetData.ExecuteQuery(query);
+            this.dgvPhoneBookList.AutoGenerateColumns = false;
+            this.dgvPhoneBookList.DataSource = Ds.Tables[0];
+        }
+
+        internal bool IsNumber(string str)
+        {
+            int Number;
+            if (int.TryParse(str, out Number))
+                return true;
+
+            return false;
+        }
+
+        internal bool IsNull()
+        {
+            if (this.txtName.Text == "") return false;
+
+
+            return true;
+        }
+
+        internal void Refresh()
+        {
+            this.txtName.Text = "";
+            this.txtNumber.Text = "";
+            this.txtEmail.Text = "";
+            this.txtAddress.Text = "";
+        }
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            // string x = txtSearch.Text.Substring(0,2);
+            try
+            {
+                if (IsNumber(txtSearch.Text))
+                {
+                    this.Sql = "select * from phonebook where contact_number LIKE '" + txtSearch.Text + "%'";
+                    this.PopulateGridView(Sql);
+                }
+                else
+                {
+                    this.Sql = "select * from phonebook where name LIKE '" + txtSearch.Text + "%'";
+                    this.PopulateGridView(Sql);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.txtName.Enabled = false;
+            this.txtNumber.Enabled = true;
+            this.txtEmail.Enabled = true;
+            this.txtAddress.Enabled = true;
+            this.dgvPhoneBookList.Enabled = true;
+            this.Refresh();
+           // this.dgvPhoneBookList_DoubleClick.Enabled = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            this.txtName.Enabled = true;
+            this.txtNumber.Enabled = true;
+            this.txtEmail.Enabled = true;
+            this.txtAddress.Enabled = true;
+            this.dgvPhoneBookList.Enabled = false;
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(this.IsNull())
+            {
+                string sql = "select * from phonebook where name = '" + this.txtName.Text + "';";
+                MessageBox.Show(sql);
+                this.Ds = this.getData.ExecuteQuery(sql);
+                if (this.Ds.Tables[0].Rows.Count == 1)
+                {
+                    sql = @"update PhoneBook set contact_number = '" + this.txtNumber.Text + "', email = '" + this.txtEmail.Text + @"',
+                      address = '" + this.txtAddress.Text + "' where name = '" + this.txtName.Text + "';";
+                    try
+                    {
+                        this.GetData.ExecuteUpdateQuery(sql);
+                        MessageBox.Show("Update Done.");
+                        this.Refresh();
+                        this.PopulateGridView();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    this.Sql = @"insert into phonebook values 
+                        ('" + txtNumber.Text + "','" + txtName.Text + "','" + txtEmail.Text + "','" + txtAddress.Text + "');";
+                    try
+                    {
+                        this.getData.ExecuteQuery(this.Sql);
+                        MessageBox.Show("Inserted");
+                        this.Refresh();
+                        this.dgvPhoneBookList.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+
+                }
+
+               
+            }
+            else
+            {
+                MessageBox.Show("Put your information please!");
+            }
+            this.PopulateGridView();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            this.dgvPhoneBookList.Enabled = true;
+            string name = Convert.ToString(this.dgvPhoneBookList.CurrentRow.Cells["name"].Value);
+            DialogResult dialogResult = MessageBox.Show("Remove " + name + " ?", "Decision", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                string sql = "delete from phonebook where name ='" + name + "';";
+                try
+                {
+                    this.getData.ExecuteQuery(sql);
+                    MessageBox.Show("Deletaion Done");
+                    this.PopulateGridView();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There is Error" + ex.Message);
+                }
+            }
+
+        }
+
+        private void dgvPhoneBookList_DoubleClick(object sender, EventArgs e)
+        {
+            this.txtName.Text = this.dgvPhoneBookList.CurrentRow.Cells["name"].Value.ToString();
+            this.txtNumber.Text = this.dgvPhoneBookList.CurrentRow.Cells["contact_number"].Value.ToString();
+            this.txtEmail.Text = this.dgvPhoneBookList.CurrentRow.Cells["email"].Value.ToString();
+            this.txtAddress.Text = this.dgvPhoneBookList.CurrentRow.Cells["address"].Value.ToString();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            this.PopulateGridView();
+            this.dgvPhoneBookList.Enabled = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPhoneBookList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
